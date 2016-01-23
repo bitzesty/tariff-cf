1795d19e6b3121b78268a52cf96a6265934b669d
Mario Andrés Correa <a@macool.me>
2016-01-22 22:20:40 -0500

    support redis via cf

--- lib/redis_client.rb
+++ lib/redis_client.rb
@@ -1,17 +1,11 @@
-require 'redis'
-
 class RedisClient
   include Singleton
 
   attr_reader :connection
 
   def initialize
-    @connection = Redis.new(config.symbolize_keys)
-  end
-
-private
-
-  def config
-    YAML.load_file(Rails.root.join("config", "redis.yml"))
+    Sidekiq.redis do |redis|
+      @connection = redis
+    end
   end
 end
